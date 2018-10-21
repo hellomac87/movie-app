8bdadb6f9bf8c5680984fb148dedd625059fddcd
hellomac87 <hellomac87@gmail.com>
2018-10-21 20:22:19 +0900

    Updates

---
+++ precache-manifest.8d06f7b7d99c2445d2f12b90632c5951.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "8a04b5025a5b565f5c28",
+    "url": "/movie_app/static/js/main.8a04b502.chunk.js"
+  },
+  {
+    "revision": "0e9123347ba41cf3d74b",
+    "url": "/movie_app/static/js/1.0e912334.chunk.js"
+  },
+  {
+    "revision": "8a04b5025a5b565f5c28",
+    "url": "/movie_app/static/css/main.1de5d165.chunk.css"
+  },
+  {
+    "revision": "237a61577a688200a5a91801743f79ca",
+    "url": "/movie_app/index.html"
+  }
+];
